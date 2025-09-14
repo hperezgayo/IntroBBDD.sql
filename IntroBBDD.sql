167b362fe19d2d34bf3e577b0fb368e1dfb2d6d8
hpgayo <68915589+hperezgayo@users.noreply.github.com>
2025-09-14 12:25:37 +0200

    Add files via upload

---
+++ IntroBBDD.sql
@@ -0,0 +1,22 @@
+SELECT flight_id, flight_no, status
+FROM flights
+WHERE status = 'On Time';
+
+SELECT *
+FROM bookings
+WHERE total_amount >= 1000000;
+
+
+SELECT *
+FROM aircrafts;
+
+
+SELECT flight_id, flight_no
+FROM flights
+WHERE aircraft_code = '733';
+
+
+SELECT *
+FROM tickets
+WHERE passenger_name ILIKE 'Irina%';
+
